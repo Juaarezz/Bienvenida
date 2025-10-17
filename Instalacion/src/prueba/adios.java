569c8e7249d1aacb43cdcb65fcae70eaeb45a447
estudiante <estudiante@oretania-PC>
2025-10-17 09:08:14 +0200

    Version 1.1

---
+++ Instalacion/src/prueba/adios.java
@@ -0,0 +1,8 @@
+package prueba;
+
+public class adios {
+	public static void main(String[] args) {
+		System.out.println("¡Adiós mundo!");
+	}
+
+}
--- Instalacion/src/prueba/demostracion.java
+++ Instalacion/src/prueba/demostracion.java
@@ -4,5 +4,7 @@ public class demostracion {
 	public static void main(String[] args) {
 		
 		System.out.println("¡Hola mundo!");
+		System.out.println("¡Hola mundo!");
+		System.out.println("¡Hola mundo!");
 	}
 }
